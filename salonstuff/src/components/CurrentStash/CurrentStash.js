04486cb91645a6e0d0f6163a5e8618da216e510d
Ethan Hind <ethanhind@gmail.com>
2018-07-17 20:26:19 -0600

    no message

--- salonstuff/src/components/CurrentStash/CurrentStash.js
+++ salonstuff/src/components/CurrentStash/CurrentStash.js
@@ -1,6 +1,6 @@
 import React from "react";
-import down from "../images/downarrow.png"
-import up from "../images/uparrow.png"
+import down from "../../images/downarrow.png"
+import up from "../../images/uparrow.png"
 
 const CurrentStash = props => (
   
@@ -11,8 +11,8 @@ const CurrentStash = props => (
       <th scope="row">{props.id}</th>
       <td>{props.itemname}</td>
       <td>{props.quantity}
-        <button type="button" class="btn btn-default btn-group-vertical" role="group"> <img src={down} width="20" /> </button>
         <button type="button" class="btn btn-default btn-group-vertical" role="group"> <img src={up} width="20" /> </button>
+        <button type="button" class="btn btn-default btn-group-vertical" role="group"> <img src={down} width="20" /> </button>
       </td>
     </tr>
 </tbody>
