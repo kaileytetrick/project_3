8a9400649f3e29e501274869dfed7facf8700968
Ethan Hind <ethanhind@gmail.com>
2018-07-19 21:09:07 -0600

    Buttons work now

--- salonstuff/src/App.js
+++ salonstuff/src/App.js
@@ -4,17 +4,61 @@ import Wrapper from "./components/Wrapper";
 import CurrentStash from "./components/CurrentStash/CurrentStash";
 // import Add from "./components/Add";
 import Footer from "./components/Footer";
-import tester from "./tester.json";
+// import tester from "./tester.json";
 // import logo from './images/logo.png';
 // import API from "./utils/API";
 // import Navbar from "./components/Navbar/Navbar";
 import Add from "./components/Add/Add";
 
 class App extends Component {
-  state = {
-    tester
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      stash: [
+        {
+          name: 'ambrosia',
+          level: '5000',
+          quantity: 7,
+          id: 1,
+          category: "color"
+        },
+        {
+          name: 40,
+          quantity: 4,
+          level: 3,
+          id: 34,
+          category: "developer"
+        },
+        {
+          quantity: 4,
+          id: 55,
+          category: "shampoo"
+        },
+        {
+          quantity: 4,
+          id: 100,
+          category: "conditioner"
+       },
+      ]
+    }  
 
+  }
+
+  getUpdatedStashFromCurrentStash = (id, newQuantity) => {
+  //   this.setState({
+  //     stash: updatedStash
+  //   })
+    var itemIndex = this.state.stash.findIndex(function(item){
+      return item.id === id
+    })
+    var tempStashState = this.state.stash;
+    console.log('newQuantity', newQuantity)
+    tempStashState[itemIndex].quantity = newQuantity
+    console.log('expected Index', itemIndex)
+    this.setState({
+      stash: tempStashState
+    })    
+  }
   
 
   render() {
@@ -25,11 +69,13 @@ class App extends Component {
       <font className="intro">Welcome to your Salon Stasher, {username}!</font><p>
       <Wrapper>
         
-      {this.state.tester.map(tester => (
+      {this.state.stash.map((item, index) => (
       <CurrentStash 
-      id={tester.id}
-      itemname={tester.itemname}
-      quantity={tester.quantity}
+      key={index}
+      id={item.id}
+      itemname={item.name ? item.name : item.category}
+      quantity={item.quantity}
+      passUpUpdatedStash={this.getUpdatedStashFromCurrentStash}
       />
       ))}
       </Wrapper>
--- salonstuff/src/components/CurrentStash/CurrentStash.js
+++ salonstuff/src/components/CurrentStash/CurrentStash.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, {Component} from "react";
 import down from "../../images/downarrow.png"
 import up from "../../images/uparrow.png"
 
-const CurrentStash = props => (
-  
-  
 
-  <tbody>
-    <tr>
-      <th scope="row">{props.id}</th>
-      <td>{props.itemname}</td>
-      <td>{props.quantity}
-        <button type="button" class="btn btn-default btn-group-vertical" role="group"> <img src={up} width="20" /> </button>
-        <button type="button" class="btn btn-default btn-group-vertical" role="group"> <img src={down} width="20" /> </button>
-      </td>
-    </tr>
-</tbody>
+//trasfer prctice data to app.js state. 
+//get date rendering from prducts on screreen again
+//onCLickevent should know what row was clicked on and update that rows qty.
 
-);
+
+
+class CurrentStash extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      
+    }
+  }
+
+  handleUpArrowClick = () => {
+    // console.log('from upArrowCLick', id)
+    this.props.passUpUpdatedStash(this.props.id, this.props.quantity +1)
+  }
+  handleDownArrowClick = () => {
+    this.props.passUpUpdatedStash(this.props.id, this.props.quantity -1)
+  }
+
+  render(){
+    return(
+      <tbody>
+        <tr>
+          <th scope="row">{this.props.id}</th>
+          <td>{this.props.itemname}</td>
+          <td>{this.props.quantity}
+            <button type="button" onClick={this.handleUpArrowClick} className="btn btn-default btn-group-vertical" role="group"> <img src={up}  alt={'up arrorw'} width="20" /> </button>
+            <button type="button" onClick={this.handleDownArrowClick} className="btn btn-default btn-group-vertical" role="group"> <img src={down} alt={'down arrow'} width="20" /> </button>
+          </td>
+        </tr>
+      </tbody>
+    )
+  }
+}
+
 
 export default CurrentStash;
