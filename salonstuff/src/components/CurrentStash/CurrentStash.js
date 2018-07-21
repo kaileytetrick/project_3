64f4217a39d9850ac90e9f41770d2dd456c8938f
salvantis <petertaillac@yahoo.com>
2018-07-20 20:59:31 -0600

    petey button

--- salonstuff/src/components/CurrentStash/CurrentStash.js
+++ salonstuff/src/components/CurrentStash/CurrentStash.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import down from "../../images/downarrow.png"
 import up from "../../images/uparrow.png"
 import "./CurrentStash.css"
+import remove from "../../images/remove.png"
 
 
 //trasfer prctice data to app.js state. 
@@ -25,6 +26,9 @@ class CurrentStash extends Component {
   handleDownArrowClick = () => {
     this.props.passUpUpdatedStash(this.props.id, this.props.quantity -1)
   }
+  handleRemoveClick = () => {
+    this.props.passUpUpdatedStash(this.props.id, this.props.quantity -100)
+  }
 
   render(){
     return(
@@ -36,6 +40,7 @@ class CurrentStash extends Component {
           <td className="align-middle">{this.props.quantity}
             <button type="button" onClick={this.handleUpArrowClick} className="btn btn-default btn-group-vertical arrows" role="group"> <img src={up}  alt={'up arrorw'} width="20" /> </button>
             <button type="button" onClick={this.handleDownArrowClick} className="btn btn-default btn-group-vertical arrows" role="group"> <img src={down} alt={'down arrow'} width="20" /> </button>
+            <button type="button" onClick={this.handleRemoveClick} className="btn btn-default btn-group-vertical remove" role="group"> <img src={remove} alt={'remove'} width="10" /> </button>
           </td>
         </tr>
       </tbody>
